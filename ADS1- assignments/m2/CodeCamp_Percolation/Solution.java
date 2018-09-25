7345a7c9adc5fddb4a39765658db5ac0d3c88ad2
manojbandari <bandarimanoj22@msitprogram.net>
2018-09-25 18:33:06 +0530

    "check_style_score -> 24 of 24 passed. style score: 0/1 "

--- ADS1- assignments/m2/CodeCamp_Percolation/Solution.java
+++ ADS1- assignments/m2/CodeCamp_Percolation/Solution.java
@@ -1,15 +1,15 @@
 // public class Percolation {
-//    public Percolation(int n)                
+//    public Percolation(int n)
 // create n-by-n grid, with all sites blocked
-//    public    void open(int row, int col)    
+//    public    void open(int row, int col)  
 // open site (row, col) if it is not open already
 //    public boolean isOpen(int row, int col)  
 // is site (row, col) open?
-//    public boolean isFull(int row, int col)  
+//    public boolean isFull(int row, int col)
 // is site (row, col) full?
-//    public     int numberOfOpenSites()       
+//    public     int numberOfOpenSites()  
 // number of open sites
-//    public boolean percolates()              
+//    public boolean percolates()       
 // does the system percolate?
 // }
 
@@ -129,7 +129,7 @@ class Percolation {
 /**
  * Class for solution.
  */
-public class Solution {
+final public class Solution {
     /**
      * Constructs the object.
      */
