54796666ed794e4b54c732548a551a8a242e3587
manojbandari <bandarimanoj22@msitprogram.net>
2018-09-25 18:24:49 +0530

    "check_style_score -> 24 of 24 passed. style score: 0/1 "

--- ADS1- assignments/m2/CodeCamp_Percolation/Solution.java
+++ ADS1- assignments/m2/CodeCamp_Percolation/Solution.java
@@ -10,61 +10,136 @@
 
 // You can implement the above API to solve the problem
 import java.util.Scanner;
-class Percolation { 
-	boolean[][] grid;
-	int size;
-	int bottom;
+
+/**
+ * Class for percolation.
+ */
+class Percolation {
+	/**
+	 * boolean 2d array.
+	 */
+	private boolean[][] grid;
+	/**
+	 * size of the grid.
+	 */
+	private int size;
+	/**
+	 * variable bottom.
+	 */
+	private int bottom;
+	/**
+	 * class variable.
+	 */
 	UF uf;
-	int count=0;
-public Percolation(int n) {
-	grid =new boolean[n][n];
-	size=n;
-	bottom=n*n+1;
-	uf = new UF(n*n+2);
+
+	/**
+	 * Constructs the object.
+	 *
+	 * @param      n     { parameter_description }
+	 */
+	public Percolation(final int n) {
+		grid = new boolean[n][n];
+		size = n;
+		bottom = n * n + 1;
+		uf = new UF(n * n + 2);
+	}
+	/**
+	 * open method.
+	 *
+	 * @param      row   The row
+	 * @param      col   The col
+	 */
+	public void open(final int row, final int col) {
+		grid[row - 1][col - 1] = true;
+		if (row == 1) {
+			uf.union(getUFIndex(row, col), 0);
+		}
+		if (row == size) {
+			uf.union(getUFIndex(row, col), bottom);
+		}
+		if (row > 1 && isOpen(row - 1, col)) {
+			uf.union(getUFIndex(row, col), getUFIndex(row - 1, col));
+		}
+		if (row < size && isOpen(row + 1, col)) {
+			uf.union(getUFIndex(row, col), getUFIndex(row + 1, col));
+		}
+		if (col > 1 && isOpen(row, col - 1)) {
+			uf.union(getUFIndex(row, col), getUFIndex(row, col - 1));
+		}
+		if (col < size && isOpen(row, col + 1)) {
+			uf.union(getUFIndex(row, col), getUFIndex(row, col + 1));
+		}
+
+	}
+
+	/**
+	 * Determines if open.
+	 *
+	 * @param      row   The row
+	 * @param      col   The col
+	 *
+	 * @return     True if open, False otherwise.
+	 */
+	public boolean isOpen(final int row, final int col) {
+		return grid[row - 1][col - 1];
+	}
+
+	/**
+	 * Determines if full.
+	 *
+	 * @param      row   The row
+	 * @param      col   The col
+	 *
+	 * @return     True if full, False otherwise.
+	 */
+	public boolean isFull(final int row, final int col) {
+		return uf.connected(0, getUFIndex(row, col));
+	}
+
+	/**
+	 * method to check percolates or not
+	 *
+	 * @return     { description_of_the_return_value }
+	 */
+	public boolean percolates() {
+		return uf.connected(0, bottom);
+	}
+
+	/**
+	 * Gets the uf index.
+	 *
+	 * @param      row   The row
+	 * @param      col   The col
+	 *
+	 * @return     The uf index.
+	 */
+	private int getUFIndex(final int row, final int col) {
+		return size * (row - 1) + col;
+	}
 }
-public void open(int row, int col) {
-	grid[row - 1][col - 1] = true;
-	if (row == 1) {
-        uf.union(getUFIndex(row, col), 0);
-    }
-    if (row == size) {
-        uf.union(getUFIndex(row, col), bottom);
-    }
-    if (row > 1 && isOpen(row - 1, col)) {
-        uf.union(getUFIndex(row, col), getUFIndex(row - 1, col));
-    }
-	if (row < size && isOpen(row + 1, col)) {
-        uf.union(getUFIndex(row, col), getUFIndex(row + 1, col));
-    }
-    if (col > 1 && isOpen(row, col - 1)) {
-        uf.union(getUFIndex(row, col), getUFIndex(row, col - 1));
-    }
-    if (col < size && isOpen(row, col + 1)) {
-        uf.union(getUFIndex(row, col), getUFIndex(row, col + 1));
-    }
-        
-}
-public boolean isOpen(int row, int col) {
-	return grid[row-1][col-1];
-}
-public boolean isFull(int row, int col) {
-	return uf.connected(0,getUFIndex(row,col));
-}
-public boolean percolates() {
-	return uf.connected(0,bottom);
-}
-private int getUFIndex(int row,int col) {
-	return size*(row-1)+col;
-}
-}
+/**
+ * Class for solution.
+ */
 public class Solution {
-	public static void main(String[] args) {
-		Scanner s= new Scanner(System.in);
-		Percolation p= new Percolation(s.nextInt());
-		while(s.hasNext()) {
-			int row= s.nextInt();
-			int col=s.nextInt();
-			p.open(row,col);
+	/**
+	 * Constructs the object.
+	 */
+	private Solution() {
+		//empty constructor
+	}
+
+	/**
+	 * main method.
+	 *
+	 * @param      args  The arguments
+	 */
+	public static void main(final String[] args) {
+		Scanner s = new Scanner(System.in);
+		Percolation p = new Percolation(s.nextInt());
+		while (s.hasNext()) {
+			int row = s.nextInt();
+			int col = s.nextInt();
+			p.open(row, col);
 		}
 		System.out.println(p.percolates());
 	}
