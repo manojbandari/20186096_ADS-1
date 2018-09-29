abcf119bbb1d46717f06522dcbe9ea4886643baa
manojbandari <bandarimanoj22@msitprogram.net>
2018-09-29 12:43:53 +0530

    "check_style_score -> 8 of 12 passed. style score: 0/1 "

--- ADS1- assignments/m6/Assignment-1/Solution.java
+++ ADS1- assignments/m6/Assignment-1/Solution.java
@@ -37,10 +37,10 @@ class AddLargeNumbers {
             if(s1.size()!=s2.size()&&(s1.empty()||s2.empty())) {
                 l.push(a);
             }
-           /* if(s1.empty()||s2.empty()) {
+            else if(s1.empty()||s2.empty()) {
                 l.push(a);
 
-            }*/
+            }
             else if(a>9) {
                 l.push(a%10);
                 temp=1;
