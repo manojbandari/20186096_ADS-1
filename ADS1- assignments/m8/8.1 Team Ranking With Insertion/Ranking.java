f75ea7544f2e69950bb6024a92910c5f856a3554
manojbandari <bandarimanoj22@msitprogram.net>
2018-10-02 20:08:18 +0530

    "check_style_score -> 6 of 6 passed. style score: 1/1 "

--- ADS1- assignments/m8/8.1 Team Ranking With Insertion/Ranking.java
+++ ADS1- assignments/m8/8.1 Team Ranking With Insertion/Ranking.java
@@ -1,31 +1,40 @@
 import java.util.Arrays;
+// class Ranking
 class Ranking {
+	// team array to store all teams
 	Team[] team;
+	// number of teams included 
 	int size;
+	// constructor
 	Ranking() {
 		team =new Team[10];
 		size=0;
 	}
+	// add teams to the team array
 	public void addteam(Team a) {
 		team[size++]=a;
 	}
+	// selection sort bases on the given conditions
+	// Order of growth for selection sort is O(N^2)
  	public void selectionSort(){
- 		for(int i=0;i<size;i++) {
+ 		for(int i=0;i<size;i++) { // complexity =N
  			int max = i;
- 			for(int j=i+1;j<size;j++) {
+ 			for(int j=i+1;j<size;j++) { // complexity = N
  				if(team[max].compareTo(team[j])==1)
  					max=j;
  			}
- 			swap(team, i, max);
+ 			swap(team, i, max); 
  		}
 
 	}
+	// swapping of two objects based on the index values in the team  array.
 	public void swap(Team[] team, int i , int max) {
 		Team temp = team[i];
 		team[i]=team[max];
 		team[max]=temp;
 
 	}
+	// print the final output based on the testcases
 	public void print() {
 		for(int i=0;i<size-1;i++) {
 			System.out.print(team[i]+",");
