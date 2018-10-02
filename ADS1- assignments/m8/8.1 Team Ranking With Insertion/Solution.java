0667496104eea6919df7638954abfdf4844479d5
manojbandari <bandarimanoj22@msitprogram.net>
2018-10-02 19:54:17 +0530

    "check_style_score -> 6 of 6 passed. style score: 0/1 "

--- ADS1- assignments/m8/8.1 Team Ranking With Insertion/Solution.java
+++ ADS1- assignments/m8/8.1 Team Ranking With Insertion/Solution.java
@@ -1,15 +1,27 @@
+/**
+ * @author: manojbandari.
+ */
 import java.util.Scanner;
+/**
+ * Class for solution with main method.
+ */
 class Solution {
-public static void main(String[] args){
+	/**
+	 * Constructs the object.
+	 */
+	private Solution() {
+		//empty constructor
+	}
+	public static void main(String[] args) {
 		Ranking r = new Ranking() ;
-		Scanner sc= new Scanner(System.in);
-		while(sc.hasNext()) {
-			String[] arr= sc.nextLine().split(",");
+		Scanner sc = new Scanner(System.in);
+		while (sc.hasNext()) {
+			String[] arr = sc.nextLine().split(",");
 			//System.out.println(Arrays.toString(arr));
 			r.addteam(new Team(arr[0],
-				Integer.parseInt(arr[1]),
-				Integer.parseInt(arr[2]),
-				Integer.parseInt(arr[3])));
+			                   Integer.parseInt(arr[1]),
+			                   Integer.parseInt(arr[2]),
+			                   Integer.parseInt(arr[2+1])));
 		}
 		r.selectionSort();
 		r.print();
