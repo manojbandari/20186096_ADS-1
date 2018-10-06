39a823977d98c47d368b07ec7b5c0d77a5cf321d
manojbandari <bandarimanoj22@msitprogram.net>
2018-10-06 12:53:53 +0530

    "ads1_m12_a1: 1/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS1- assignments/m12/Assignment-1/Solution.java
+++ ADS1- assignments/m12/Assignment-1/Solution.java
@@ -159,11 +159,11 @@ class Sorting{
 					finalstudents[k++]=students[i];
 					size1++;
 				}
-				/*else if(students[i].getReservation().equals("Open")&&oCount!=1) {
-					oCount+=1;
+				else if(students[i].getReservation().equals("Open")&&stCount==0&&oCount==0) {
+					oCount++;
 					size1++;
 					finalstudents[k++]=students[i];
-				}*/
+				}
 				else if(bCount==bcCategory&&sCount==scCategory&&stCount==stCategory&&size1!=totalVacancies) {
 					finalstudents[k++]=students[i];
 				}
