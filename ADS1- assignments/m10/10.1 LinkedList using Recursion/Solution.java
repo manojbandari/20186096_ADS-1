e588ce1b36909f5cf4b865acbdb04ed9c6388804
manojbandari <bandarimanoj22@msitprogram.net>
2018-10-07 21:46:46 +0530

    "ads1_m10_a1: 3/3 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS1- assignments/m10/10.1 LinkedList using Recursion/Solution.java
+++ ADS1- assignments/m10/10.1 LinkedList using Recursion/Solution.java
@@ -1,70 +1,152 @@
 import java.util.Scanner;
-import java.util.Arrays;
-class Solution {
-	public static void main(String[] args) {
-		LinkedListRecursive<Integer> l = new LinkedListRecursive<Integer>();
-		Scanner s= new Scanner(System.in);
-		while(s.hasNext()) {
-			String[] tokens = s.nextLine().split(" ");
-			switch(tokens[0]) {
-				case "InsertAt":
-				System.out.println("InsertAt");
-					l.insertAt(Integer.parseInt(tokens[1], Integer.parseInt(tokens[2])));
-				break;
-				case "reverse":
-				System.out.println("reverse");
-				l.reverse();
-
-				break;
-				default:
-					break;
-			}
-		}
-	}
+/**
+ * List of linkeds.
+ */
+class LinkedList {
+    /**
+     * Class for node.
+     */
+    class Node {
+        /**
+         * value of node.
+         */
+        private int value;
+        /**
+         * address fo node.
+         */
+        private Node address;
+        /**
+         * Constructs the object.
+         *
+         * @param      val   The value
+         */
+        Node(final int val) {
+            value = val;
+        }
+    }
+    /**
+     * head of Node.
+     */
+    private Node head;
+    /**
+     * size.
+     */
+    private int size;
+    /**
+     * Constructs the object.
+     */
+    LinkedList() {
+        head = null;
+        size = 0;
+    }
+    /**
+     * insert at any position.
+     *
+     * @param      pos        The position.
+     * @param      ele        The ele.
+     *
+     * @throws     Exception  { handling exception }.
+     */
+    public void insertAt(final int pos, final int ele) throws Exception {
+        Node newNode = new Node(ele);
+        if (pos < 0 || pos > size) {
+            throw new Exception();
+        }
+        head = insertAt(pos, head, newNode, 0);
+    }
+    /**
+     * insert at any position having overiding.
+     *
+     * @param      pos    The position
+     * @param      first  The first
+     * @param      obj    The object
+     * @param      count  The count
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public Node insertAt(final int pos, final Node first,
+                         final Node obj, final int count) {
+        if (pos == count) {
+            obj.address = first;
+            size++;
+            return obj;
+        }
+        first.address = insertAt(pos, first.address, obj, count + 1);
+        return first;
+    }
+    /**
+     * reverse.
+     */
+    public void reverse() {
+        reverse(null, head);
+    }
+    /**
+     * reverse the node.
+     *
+     * @param      previous  The previous.
+     * @param      current   The current.
+     */
+    void reverse(final Node previous, final Node current) {
+        if (current != null) {
+            reverse(current, current.address);
+            current.address = previous;
+        } else {
+            head = previous;
+        }
+    }
+    /**
+     * display the node.
+     */
+    void display() {
+        Node temp = head;
+        String str = "";
+        while (temp != null) {
+            str += temp.value + ", ";
+            temp = temp.address;
+        }
+        System.out.println(str.substring(0, str.length() - 2));
+    }
 }
-class LinkedListRecursive<E> {
-	int size =0;
-	private class Node {
-		private E data;
-		private Node next;
-		Node(E data, Node next) {
-			this.data = data;
-			this.next=next;
-		}
-	}
-	Node head;
-	//Node tail=null;
-	public void insertAt(int index, E dat) {
-		head = insertHelper(head,index,dat);
-	}
-	void reverse() {
-		head= reverseHelper(head);
-	}
-	Node reverseHelper(Node head) {
-		if(head==null || head.next==null) return head;
-		Node nhead = reverseHelper(head.next);
-		head.next.next=head;
-		head.next=null;
-		return head;
- 	}
-	Node insertHelper(Node head, int count, E dat) {
-		if(count==0) return new Node(dat, head);
-		head.next = insertHelper(head.next,cnt-1,dat);
-		return head;
-	}
-	public E next() {
-		E data = current.data;
-		current = current.next;
-		return data;
-	}
-	public Iterator iterator() {
-		return new MyIterator(head);
-	}
-	private class MyIterator implements Iterator {
-		Node current;
-		public MyIterator(Node first) {
-			current = first;
-		}
-		public boolean hasNext() {}
-	}
+/**
+ * { main class }.
+ */
+public final class Solution {
+    /**
+     * Constructs the object.
+     */
+    private Solution() { }
+    /**
+     * main class.
+     *
+     * @param      args  The arguments.
+     */
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        LinkedList l = new LinkedList();
+        while (sc.hasNext()) {
+            String data = sc.nextLine();
+            String[] tokens = data.split(" ");
+            switch (tokens[0]) {
+            case "insertAt":
+                try {
+                    l.insertAt(Integer.parseInt(tokens[1]),
+                               Integer.parseInt(tokens[2]));
+                    l.display();
+                } catch (Exception e) {
+                    System.out.println("Can't insert at this position.");
+                }
+                break;
+            case "reverse":
+                try {
+                    l.reverse();
+                    l.display();
+                } catch (Exception e) {
+                    System.out.println("No elements to reverse.");
+                }
+                break;
+            default:
+                break;
+            }
+        }
+    }
 }
